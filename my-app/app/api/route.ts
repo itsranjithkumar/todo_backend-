0494f6f3278bb127fa2944c2223d161e8ea7cd33
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2024-12-25 14:04:43 +0530

    adsda

---
+++ my-app/app/api/route.ts
@@ -0,0 +1,6 @@
+// app/api/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({ message: 'Hello World' }, { status: 200 });
+}
